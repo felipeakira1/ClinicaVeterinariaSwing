f906cd0a71aa075786c0c1fdc57ba8008534e312
felipeakira-exe <feakinoz@gmail.com>
2024-10-24 14:09:19 -0300

    feat: add update, delete and search functions to tutor form

--- src/view/forms/TutorForm.java
+++ src/view/forms/TutorForm.java
@@ -31,15 +31,17 @@ public class TutorForm extends javax.swing.JPanel {
         Utils.updateTableModel(tableTutores, tutorController.getAllTutores(), tutorTableModel);
     }
     
-    private void initMask() {
-        
-    }
     private void initPanel() {
         txtTutorCpf.setColumns(14);
         Utils.setComponentsEnabled(true, btnNovoTutor);
         Utils.setComponentsEnabled(true, btnNovoTutor);
         Utils.setComponentsEnabled(false, btnAlterarTutor, btnExcluirTutor, btnSalvarTutor, btnCancelarTutor);
         Utils.setComponentsEnabled(false, txtTutorId, txtTutorNome, txtTutorEndereco, txtTutorCpf);
+    }
+    
+    private void updateTable(JTable table, List<Tutor> tutores) {
+        TutorTableModel model = new TutorTableModel(tutores);
+        table.setModel(model);
     }
     
     @SuppressWarnings("unchecked")
@@ -65,12 +67,27 @@ public class TutorForm extends javax.swing.JPanel {
         jPanel4 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         tableTutores = new javax.swing.JTable();
+        txtPesquisarTutor = new javax.swing.JTextField();
+        btnPesquisarTutor = new view.components.SearchButton();
+        jLabel5 = new javax.swing.JLabel();
 
         setPreferredSize(new java.awt.Dimension(800, 800));
 
         btnNovoTutor.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnNovoTutorActionPerformed(evt);
+            }
+        });
+
+        btnAlterarTutor.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAlterarTutorActionPerformed(evt);
+            }
+        });
+
+        btnExcluirTutor.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnExcluirTutorActionPerformed(evt);
             }
         });
 
@@ -190,7 +207,7 @@ public class TutorForm extends javax.swing.JPanel {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-        jPanel4.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Pesquisar tutores", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Segoe UI", 1, 14))); // NOI18N
+        jPanel4.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Pesquisar tutores por nome", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Segoe UI", 1, 14))); // NOI18N
 
         tableTutores.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -210,19 +227,37 @@ public class TutorForm extends javax.swing.JPanel {
         });
         jScrollPane1.setViewportView(tableTutores);
 
+        btnPesquisarTutor.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPesquisarTutorActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
         jPanel4.setLayout(jPanel4Layout);
         jPanel4Layout.setHorizontalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 777, Short.MAX_VALUE)
+            .addGroup(jPanel4Layout.createSequentialGroup()
+                .addComponent(txtPesquisarTutor, javax.swing.GroupLayout.PREFERRED_SIZE, 331, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(24, 24, 24)
+                .addComponent(btnPesquisarTutor, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel4Layout.setVerticalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel4Layout.createSequentialGroup()
-                .addGap(64, 64, 64)
+                .addContainerGap()
+                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtPesquisarTutor, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnPesquisarTutor, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 203, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
+
+        jLabel5.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel5.setText("Tutores");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -239,41 +274,57 @@ public class TutorForm extends javax.swing.JPanel {
                         .addContainerGap()
                         .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                 .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel5)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
+                .addGap(11, 11, 11)
+                .addComponent(jLabel5)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(179, Short.MAX_VALUE))
+                .addContainerGap(164, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnSalvarTutorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSalvarTutorActionPerformed
+        String idTxt = txtTutorId.getText();
         String nome = txtTutorNome.getText();
         String endereco = txtTutorEndereco.getText();
         String cpf = txtTutorCpf.getText();
         if (nome.isEmpty() || endereco.isEmpty() || cpf.isEmpty()) {
             Utils.showWarningMessage("Campos obrigatórios", "Por favor, preencha todos os campos obrigatórios");
-        } else {
-            tutorController.createTutor(nome, endereco, cpf);
-            Utils.showSuccessfulMessage();
-            txtTutorNome.setText("");
-            txtTutorEndereco.setText("");
-            txtTutorCpf.setText("");
-            Utils.setComponentsEnabled(false, txtTutorNome, txtTutorEndereco, txtTutorCpf);                             
-            Utils.setComponentsEnabled(true, btnNovoTutor);
-            Utils.setComponentsEnabled(false, btnSalvarTutor, btnCancelarTutor);
-            tutorTableModel.setData(tutorController.getAllTutores());
-            tableTutores.setModel(tutorTableModel);
-            Utils.updateTableModel(tableTutores, tutorController.getAllTutores(), tutorTableModel);
+            return;
         }
+        if(idTxt.isEmpty()){
+            tutorController.createTutor(nome, endereco, cpf);
+        } else {
+            int id = Integer.parseInt(idTxt);
+            Tutor tutor = tutorController.getTutorById(id);
+            if(tutor != null) {
+                tutor.setNome(nome);
+                tutor.setEndereco(endereco);
+                tutorController.updateTutor(tutor);
+            }
+        }
+        Utils.showSuccessfulMessage();
+        txtTutorId.setText("");
+        txtTutorNome.setText("");
+        txtTutorEndereco.setText("");
+        txtTutorCpf.setText("");
+        Utils.setComponentsEnabled(false, txtTutorNome, txtTutorEndereco, txtTutorCpf);                             
+        Utils.setComponentsEnabled(true, btnNovoTutor);
+        Utils.setComponentsEnabled(false, btnSalvarTutor, btnCancelarTutor);
+        updateTable(tableTutores, tutorController.getAllTutores());
     }//GEN-LAST:event_btnSalvarTutorActionPerformed
     
     private void btnNovoTutorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnNovoTutorActionPerformed
@@ -294,17 +345,45 @@ public class TutorForm extends javax.swing.JPanel {
     }//GEN-LAST:event_btnCancelarTutorActionPerformed
 
     private void tableTutoresMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tableTutoresMouseClicked
-        Utils.setComponentsEnabled(false, btnNovoTutor);
-        Utils.setComponentsEnabled(true, btnAlterarTutor, btnExcluirTutor, btnCancelarTutor);
-        int row = tableTutores.getSelectedRow();
-        if(row != -1) {
-            Tutor tutor = ((TutorTableModel)tableTutores.getModel()).getItem(row);
-            txtTutorId.setText(String.valueOf(tutor.getId()));
-            txtTutorNome.setText(tutor.getNome());
-            txtTutorEndereco.setText(tutor.getEndereco());
-            txtTutorCpf.setText(tutor.getCpf());
+        if(!txtTutorNome.isEnabled()) { // Verify if there is any data in the form
+            Utils.setComponentsEnabled(false, btnNovoTutor);
+            Utils.setComponentsEnabled(true, btnAlterarTutor, btnExcluirTutor, btnCancelarTutor);
+            int row = tableTutores.getSelectedRow();
+            if(row != -1) {
+                Tutor tutor = ((TutorTableModel)tableTutores.getModel()).getItem(row);
+                txtTutorId.setText(String.valueOf(tutor.getId()));
+                txtTutorNome.setText(tutor.getNome());
+                txtTutorEndereco.setText(tutor.getEndereco());
+                txtTutorCpf.setText(tutor.getCpf());
+            }
         }
     }//GEN-LAST:event_tableTutoresMouseClicked
+
+    private void btnAlterarTutorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAlterarTutorActionPerformed
+        Utils.setComponentsEnabled(false, btnNovoTutor, btnAlterarTutor, btnExcluirTutor);                                             
+        Utils.setComponentsEnabled(true, btnSalvarTutor);
+        Utils.setComponentsEnabled(true, txtTutorNome, txtTutorEndereco, txtTutorCpf);
+    }//GEN-LAST:event_btnAlterarTutorActionPerformed
+
+    private void btnExcluirTutorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnExcluirTutorActionPerformed
+        String idTxt = txtTutorId.getText();
+        int id = Integer.parseInt(idTxt);
+        Tutor tutor = tutorController.getTutorById(id);
+        tutorController.deleteTutor(tutor);
+        Utils.showSuccessfulMessage();
+        txtTutorId.setText("");
+        txtTutorNome.setText("");
+        txtTutorEndereco.setText("");
+        txtTutorCpf.setText("");
+        Utils.setComponentsEnabled(true, btnNovoTutor);
+        Utils.setComponentsEnabled(false, btnAlterarTutor, btnExcluirTutor, btnSalvarTutor, btnCancelarTutor);
+        updateTable(tableTutores, tutorController.getAllTutores());
+    }//GEN-LAST:event_btnExcluirTutorActionPerformed
+
+    private void btnPesquisarTutorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPesquisarTutorActionPerformed
+        String nome = txtPesquisarTutor.getText();
+        updateTable(tableTutores, tutorController.searchTutorByName(nome));
+    }//GEN-LAST:event_btnPesquisarTutorActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -312,17 +391,20 @@ public class TutorForm extends javax.swing.JPanel {
     private view.components.CancelButton btnCancelarTutor;
     private view.components.DeleteButton btnExcluirTutor;
     private view.components.NewButton btnNovoTutor;
+    private view.components.SearchButton btnPesquisarTutor;
     private view.components.SaveButton btnSalvarTutor;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tableTutores;
+    private javax.swing.JTextField txtPesquisarTutor;
     private javax.swing.JFormattedTextField txtTutorCpf;
     private javax.swing.JTextField txtTutorEndereco;
     private javax.swing.JTextField txtTutorId;
